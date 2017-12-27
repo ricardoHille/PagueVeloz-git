9f1298a77d5434b4ff0995b52a9548776f42330b
ricardoHille <ricardo_rfh@hotmail.com>
2017-12-27 01:03:30 -0200

    Inclusão de referências

---
+++ PagueVeloz/MVC/Models/Enum/EnumEstado.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Models.Enum
+{
+    public enum EnumEstado
+    {
+        SantaCatarina = 1,
+        Parana = 2,
+    }
+}
